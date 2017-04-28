328a8aafbc5e950c0405ee12cb93c7ac1c7a3b7d
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-04-28 12:19:08 +0200

    Add package.json with the needed packages installed. Start creating index.js.

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mgab on 28/04/2017.
+ */
+var chalk       = require('chalk');
+var clear       = require('clear');
+var CLI         = require('clui');
+var figlet      = require('figlet');
+var inquirer    = require('inquirer');
+var Preferences = require('preferences');
+var Spinner     = CLI.Spinner;
+var GitHubApi   = require('github');
+var _           = require('lodash');
+var git         = require('simple-git')();
+var touch       = require('touch');
+var fs          = require('fs');
+
