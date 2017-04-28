afe75636ead40014d78d044b0e1c4716b020f42e
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-04-28 14:22:02 +0200

    Added inquirer prompt for user and pass.

--- index.js
+++ index.js
@@ -22,3 +22,38 @@ if (files.directoryExists('.git')) {
     console.log(chalk.red('Already a git repository!'));
     process.exit();
 }
+
+function getGithubCredentials(callback) {
+    var questions = [
+        {
+            name: 'username',
+            type: 'input',
+            message: 'Enter your Github username or e-mail address:',
+            validate: function( value ) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter your username or e-mail address';
+                }
+            }
+        },
+        {
+            name: 'password',
+            type: 'password',
+            message: 'Enter your password:',
+            validate: function(value) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter your password';
+                }
+            }
+        }
+    ];
+
+    inquirer.prompt(questions).then(callback);
+}
+
+getGithubCredentials(function() {
+    console.log(arguments);
+});
