43848545e0db26e872d01c2330b780dd900dab27
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-04-28 14:43:27 +0200

    Added creating repo func.

--- index.js
+++ index.js
@@ -57,3 +57,103 @@ function getGithubCredentials(callback) {
 getGithubCredentials(function() {
     console.log(arguments);
 });
+
+var prefs = new Preferences('ginit');
+
+var github = new GitHubApi({
+    version: '3.0.0'
+});
+
+function getGithubToken(callback) {
+    var prefs = new Preferences('ginit');
+
+    if (prefs.github && prefs.github.token) {
+        return callback(null, prefs.github.token);
+    }
+
+    getGithubCredentials(function(credentials) {
+        var status = new Spinner('Authenticating you, please wait...');
+        status.start();
+
+        github.authenticate(
+            _.extend(
+                {
+                    type: 'basic',
+                },
+                credentials
+            )
+        );
+
+        github.authorization.create({
+            scopes: ['user', 'public_repo', 'repo', 'repo:status'],
+            note: 'ginit, the command-line tool for initalizing Git repos'
+        }, function(err, res) {
+            status.stop();
+            if ( err ) {
+                return callback( err );
+            }
+            if (res.token) {
+                prefs.github = {
+                    token : res.token
+                };
+                return callback(null, res.token);
+            }
+            return callback();
+        });
+    });
+}
+
+function createRepo(callback) {
+    var argv = require('minimist')(process.argv.slice(2));
+
+    var questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter a name for the repository:',
+            default: argv._[0] || files.getCurrentDirectoryBase(),
+            validate: function( value ) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter a name for the repository';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            default: argv._[1] || null,
+            message: 'Optionally enter a description of the repository:'
+        },
+        {
+            type: 'list',
+            name: 'visibility',
+            message: 'Public or private:',
+            choices: [ 'public', 'private' ],
+            default: 'public'
+        }
+    ];
+
+    inquirer.prompt(questions).then(function(answers) {
+        var status = new Spinner('Creating repository...');
+        status.start();
+
+        var data = {
+            name : answers.name,
+            description : answers.description,
+            private : (answers.visibility === 'private')
+        };
+
+        github.repos.create(
+            data,
+            function(err, res) {
+                status.stop();
+                if (err) {
+                    return callback(err);
+                }
+                return callback(null, res.ssh_url);
+            }
+        );
+    });
+}
