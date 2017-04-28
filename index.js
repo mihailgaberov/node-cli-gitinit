95f9c7a296d1b3fe28807c0f7d63a58b47c3e187
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-04-28 16:21:28 +0200

    Finished index.js - main func.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var fs = require('fs');
 var files = require('./lib/files');
 
 clear();
-console.log(chalk.yellow(figlet.textSync('Ginit', { horizontalLayout: 'full' })));
+console.log(chalk.yellow(figlet.textSync('Ginit', {horizontalLayout: 'full'})));
 
 if (files.directoryExists('.git')) {
     console.log(chalk.red('Already a git repository!'));
@@ -29,7 +29,7 @@ function getGithubCredentials(callback) {
             name: 'username',
             type: 'input',
             message: 'Enter your Github username or e-mail address:',
-            validate: function( value ) {
+            validate: function (value) {
                 if (value.length) {
                     return true;
                 } else {
@@ -41,7 +41,7 @@ function getGithubCredentials(callback) {
             name: 'password',
             type: 'password',
             message: 'Enter your password:',
-            validate: function(value) {
+            validate: function (value) {
                 if (value.length) {
                     return true;
                 } else {
@@ -54,11 +54,9 @@ function getGithubCredentials(callback) {
     inquirer.prompt(questions).then(callback);
 }
 
-getGithubCredentials(function() {
+getGithubCredentials(function () {
     console.log(arguments);
 });
-
-var prefs = new Preferences('ginit');
 
 var github = new GitHubApi({
     version: '3.0.0'
@@ -71,7 +69,7 @@ function getGithubToken(callback) {
         return callback(null, prefs.github.token);
     }
 
-    getGithubCredentials(function(credentials) {
+    getGithubCredentials(function (credentials) {
         var status = new Spinner('Authenticating you, please wait...');
         status.start();
 
@@ -87,14 +85,14 @@ function getGithubToken(callback) {
         github.authorization.create({
             scopes: ['user', 'public_repo', 'repo', 'repo:status'],
             note: 'ginit, the command-line tool for initalizing Git repos'
-        }, function(err, res) {
+        }, function (err, res) {
             status.stop();
-            if ( err ) {
-                return callback( err );
+            if (err) {
+                return callback(err);
             }
             if (res.token) {
                 prefs.github = {
-                    token : res.token
+                    token: res.token
                 };
                 return callback(null, res.token);
             }
@@ -112,7 +110,7 @@ function createRepo(callback) {
             name: 'name',
             message: 'Enter a name for the repository:',
             default: argv._[0] || files.getCurrentDirectoryBase(),
-            validate: function( value ) {
+            validate: function (value) {
                 if (value.length) {
                     return true;
                 } else {
@@ -130,24 +128,24 @@ function createRepo(callback) {
             type: 'list',
             name: 'visibility',
             message: 'Public or private:',
-            choices: [ 'public', 'private' ],
+            choices: ['public', 'private'],
             default: 'public'
         }
     ];
 
-    inquirer.prompt(questions).then(function(answers) {
+    inquirer.prompt(questions).then(function (answers) {
         var status = new Spinner('Creating repository...');
         status.start();
 
         var data = {
-            name : answers.name,
-            description : answers.description,
-            private : (answers.visibility === 'private')
+            name: answers.name,
+            description: answers.description,
+            private: (answers.visibility === 'private')
         };
 
         github.repos.create(
             data,
-            function(err, res) {
+            function (err, res) {
                 status.stop();
                 if (err) {
                     return callback(err);
@@ -157,3 +155,92 @@ function createRepo(callback) {
         );
     });
 }
+
+function createGitignore(callback) {
+    var filelist = _.without(fs.readdirSync('.'), '.git', '.gitignore');
+
+    if (filelist.length) {
+        inquirer.prompt(
+            [
+                {
+                    type: 'checkbox',
+                    name: 'ignore',
+                    message: 'Select the files and/or folders you wish to ignore:',
+                    choices: filelist,
+                    default: ['node_modules', 'bower_components']
+                }
+            ]
+        ).then(function (answers) {
+                if (answers.ignore.length) {
+                    fs.writeFileSync('.gitignore', answers.ignore.join('\n'));
+                } else {
+                    touch('.gitignore');
+                }
+                return callback();
+            }
+        );
+    } else {
+        touch('.gitignore');
+        return callback();
+    }
+}
+
+function setupRepo( url, callback ) {
+    var status = new Spinner('Setting up the repository...');
+    status.start();
+
+    git
+        .init()
+        .add('.gitignore')
+        .add('./*')
+        .commit('Initial commit')
+        .addRemote('origin', url)
+        .push('origin', 'master')
+        .then(function(){
+            status.stop();
+            return callback();
+        });
+}
+
+function githubAuth(callback) {
+    getGithubToken(function(err, token) {
+        if (err) {
+            return callback(err);
+        }
+        github.authenticate({
+            type : 'oauth',
+            token : token
+        });
+        return callback(null, token);
+    });
+}
+
+githubAuth(function(err, authed) {
+    if (err) {
+        switch (err.code) {
+            case 401:
+                console.log(chalk.red('Couldn\'t log you in. Please try again.'));
+                break;
+            case 422:
+                console.log(chalk.red('You already have an access token.'));
+                break;
+        }
+    }
+    if (authed) {
+        console.log(chalk.green('Sucessfully authenticated!'));
+        createRepo(function(err, url){
+            if (err) {
+                console.log('An error has occured');
+            }
+            if (url) {
+                createGitignore(function() {
+                    setupRepo(url, function(err) {
+                        if (!err) {
+                            console.log(chalk.green('All done!'));
+                        }
+                    });
+                });
+            }
+        });
+    }
+});
