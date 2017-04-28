64c2b93fbcb5654d48896c5733fae39542c282f7
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-04-28 13:30:42 +0200

    Added first colorful text to the console.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /**
  * Created by mgab on 28/04/2017.
  */
-var chalk       = require('chalk');
-var clear       = require('clear');
-var CLI         = require('clui');
-var figlet      = require('figlet');
-var inquirer    = require('inquirer');
+var chalk = require('chalk');
+var clear = require('clear');
+var CLI = require('clui');
+var figlet = require('figlet');
+var inquirer = require('inquirer');
 var Preferences = require('preferences');
-var Spinner     = CLI.Spinner;
-var GitHubApi   = require('github');
-var _           = require('lodash');
-var git         = require('simple-git')();
-var touch       = require('touch');
-var fs          = require('fs');
+var Spinner = CLI.Spinner;
+var GitHubApi = require('github');
+var _ = require('lodash');
+var git = require('simple-git')();
+var touch = require('touch');
+var fs = require('fs');
+var files = require('./lib/files');
 
+clear();
+console.log(chalk.yellow(figlet.textSync('Ginit', { horizontalLayout: 'full' })));
